d76f51278f4aac540ac7b87f7b566d2812b2931f
Phoebe <phoebe.freeston@biology.ox.ac.uk>
2026-05-05 11:14:42 +0100

    updating paths in eva_data_analysis.py

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,7 +1,7 @@
 # https://data.nasa.gov/resource/eva.json (with modifications)
-data_f = open('/home/sarah/Projects/astronaut-analysis/data.json', 'r')
-data_t = open('/home/sarah/Projects/astronaut-analysis/data.csv','w')
-g_file = 'myplot.png'
+data_f = open('spacewalks/eva-data.json', 'r')
+data_t = open('spacewalks/data.csv','w')
+g_file = 'cumulative_eva_graph.png'
 
 fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
