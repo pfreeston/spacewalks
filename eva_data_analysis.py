247eab29f6a88392588be617df6ede41abaf03e9
Phoebe <phoebe.freeston@biology.ox.ac.uk>
2026-05-07 12:15:33 +0100

    fix bug and other additions

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import re
 
 '''
 This script reads in data about extravehicular activities (EVAs) from a JSON file, 
@@ -13,19 +14,21 @@ hours, calculates the cumulative time, and plots it against the date.
 '''
 
 def main(input_file, output_file, graph_file):
-
     print("--START--")
 
-    # Read the data from the json file
+    # Read the data from JSON file
     eva_data = read_json_to_dataframe(input_file)
 
-    # Convert and export the data to a csv file
+    # Calculate and add crew size to data
+    eva_data = add_crew_size_column(eva_data) # added this line
+
+    # Convert and export data to CSV file
     write_dataframe_to_csv(eva_data, output_file)
 
-    # Sort the DataFrame by date, convert 'duration' to hours, and calculate the cumulative time spent in space
+    # Sort dataframe by date ready to be plotted (date values are on x-axis)
     eva_data.sort_values('date', inplace=True)
 
-    # Plot the cumulative time spent in space over the years and save the plot to a file
+    # Plot cumulative time spent in space over years
     plot_cumulative_time_in_space(eva_data, graph_file)
 
     print("--END--")
@@ -96,7 +99,7 @@ def text_to_duration(duration):
         duration_hours (float): The duration in hours
     """
     hours, minutes = duration.split(":")
-    duration_hours = int(hours) + int(minutes)/6  # there is an intentional bug on this line (should divide by 60 not 6)
+    duration_hours = int(hours) + int(minutes)/60  # there is an intentional bug on this line (should divide by 60 not 6)
     return duration_hours
 
 
@@ -113,6 +116,40 @@ def add_duration_hours(df):
     df_copy = df.copy()
     df_copy["duration_hours"] = df_copy["duration"].apply(
         text_to_duration
+    )
+    return df_copy
+
+
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+    
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
     )
     return df_copy
 
